2b7a82e765f140acaad240f1926391e22d0d6039
Daniel Xavier <dxdanielxavier@gmail.com>
2025-12-31 14:45:48 -0300

    feat: added header home link

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import { Montserrat } from "next/font/google";
 
+import Link from "next/link";
+
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -23,7 +25,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${montserrat.variable} antialiased`}>
         <header className="py-[7px] fixed w-full flex items-center justify-center bg-background shadow-[0_1px_0_0_var(--color-green-teal)]">
-          <h1 className="text-[9px] font-bold text-green-teal-2">SWStarter</h1>
+          <h1 className="text-[9px] font-bold text-green-teal-2">
+            <Link href="/">SWStarter</Link>
+          </h1>
         </header>
 
         <main className="py-[28px] bg-[#ededed] min-h-screen">{children}</main>
